b4e9d36c0d77c5bfec4c0f25edb6328258fd1a69
Ming-Yeung-Alfred-Meng <alfred845301120@gmail.com>
2023-04-11 12:24:26 -0400

    accuracies_after_fine_tuning() and losses_after_fine_tuning().

--- scripts.py
+++ scripts.py
@@ -66,10 +66,59 @@ Freeze a number of layers in the model.
 """
 def freeze(model: tf.keras.Model,
            number_of_layers_to_freeze: int) -> None:
-        
+    
     for layer in model.layers[:number_of_layers_to_freeze]:
         layer.trainable = False
 
+
+"""
+Plot accuracies after fine-tuning.
+"""
+def accuracies_after_fine_tuning(classifier_history: tf.keras.callbacks.History,
+                                 fine_tuning_history: tf.keras.callbacks.History,
+                                 classifier_number_of_epoches: int,
+                                 title: str,
+                                 figsize: Optional[tuple[int, int]] = None) -> None:
+
+    trianing_accuracy = classifier_history.history['accuracy'] + fine_tuning_history.history['accuracy']
+    validation_accuracy = classifier_history.history['val_accuracy'] + fine_tuning_history.history['val_accuracy']
+
+    figure, axis = plt.subplots(figsize=figsize)
+    axis.plot(trianing_accuracy, label='Training Accuracy')
+    axis.plot(validation_accuracy, label='Validation Accuracy')
+    axis.set_ylim(0.8, 1)
+    axis.plot([classifier_number_of_epoches - 1, classifier_number_of_epoches - 1],
+              axis.get_ylim(), label='Start Fine Tuning')
+    axis.set_xlabel('Epoch')
+    axis.set_ylabel('Accuracy')
+    axis.legend()
+    figure.suptitle(title)
+
+
+"""
+Plot losses after fine-tuning.
+"""
+def losses_after_fine_tuning(classifier_history: tf.keras.callbacks.History,
+                             fine_tuning_history: tf.keras.callbacks.History,
+                             classifier_number_of_epoches: int,
+                             title: str,
+                             figsize: Optional[tuple[int, int]] = None) -> None:
+
+    trianing_loss = classifier_history.history['loss'] + fine_tuning_history.history['loss']
+    validation_loss = classifier_history.history['val_loss'] + fine_tuning_history.history['val_loss']
+
+    figure, axis = plt.subplots(figsize=figsize)
+
+    axis.plot(trianing_loss, label='Training Loss')
+    axis.plot(validation_loss, label='Validation Loss')
+    axis.set_ylim(0, 1.0)
+    axis.plot([classifier_number_of_epoches - 1, classifier_number_of_epoches - 1],
+              axis.get_ylim(), label='Start Fine Tuning')
+    axis.legend()
+    figure.suptitle('Training and Validation Loss')
+    axis.set_xlabel('Epoch')
+    axis.set_ylabel('Cross Entropy')
+    
 
 # """
 # Return a pre-trained model without the last classification layer.
